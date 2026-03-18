30682b3a96da53403d73e38316f59f7f8ca4831d
HalloWelt42 <my@jobmagnetix.de>
2026-03-18 23:59:48 +0100

    Shield-Zahl: Orbitron-Font (technischer Sci-Fi-Look)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,8 @@
 import '@fontsource-variable/inter'
 import '@fontsource/jetbrains-mono/400.css'
 import '@fontsource/jetbrains-mono/500.css'
+import '@fontsource/orbitron/700.css'
+import '@fontsource/orbitron/900.css'
 import './app.css'
 import { mount } from 'svelte'
 import App from './App.svelte'
